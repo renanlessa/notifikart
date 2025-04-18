5490f85bd23bd70c3932132b69ce73924a3457a8
Renan Lessa <renanlessa@gmail.com>
2025-04-17 21:41:56 -0300

    Initialize notification and order services with Spring Boot setup

---
+++ notification-service/src/main/java/dev/java10x/notification/NotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.java10x.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationServiceApplication.class, args);
+	}
+
+}
