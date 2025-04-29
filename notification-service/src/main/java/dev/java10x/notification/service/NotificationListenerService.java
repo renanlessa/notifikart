ecbdebe022ced1308a2f7ecb3f256b3aabf41260
Renan Lessa <renanlessa@gmail.com>
2025-04-29 17:39:08 -0300

    Rename KafkaConsumerService to NotificationListenerService for clarity

--- notification-service/src/main/java/dev/java10x/notification/service/NotificationListenerService.java
+++ notification-service/src/main/java/dev/java10x/notification/service/NotificationListenerService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
-public class KafkaConsumerService {
+public class NotificationListenerService {
 
     @KafkaListener(topics = "${kafka.topic}")
     public void consumeMessage(NotificationMessage notificationMesssage) {
